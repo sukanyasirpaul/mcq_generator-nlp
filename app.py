02ca46f26634b43fda867492eb349cb2fa357039
sukanyasirpaul <ssirpaul_msc23@thapar.edu>
2026-03-03 00:50:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,27 +5,14 @@ st.set_page_config(page_title="Automated MCQ Generator")
 
 st.title("📘 Automated MCQ Generator using NLP")
 
-st.write("Generate MCQs from text or upload file")
-
-# Text input
 text_input = st.text_area("Enter paragraph here:")
-
-# File upload
-uploaded_file = st.file_uploader("Or upload a .txt file", type=["txt"])
-
 num_questions = st.slider("Number of MCQs", 1, 10, 3)
 
 if st.button("Generate MCQs"):
+    if text_input.strip() != "":
+        mcqs = generate_mcqs(text_input, num_questions)
 
-    if uploaded_file is not None:
-        text = uploaded_file.read().decode("utf-8")
-    else:
-        text = text_input
-
-    if text:
-        mcqs = generate_mcqs(text, num_questions)
-
-        if mcqs:
+        if len(mcqs) > 0:
             for i, mcq in enumerate(mcqs):
                 st.subheader(f"Question {i+1}")
                 st.write(mcq["question"])
@@ -34,9 +21,7 @@ if st.button("Generate MCQs"):
                     st.write(f"- {option}")
 
                 st.success(f"Correct Answer: {mcq['answer']}")
-
         else:
             st.error("Not enough content to generate MCQs.")
-
     else:
-        st.warning("Please enter text or upload file.")
+        st.warning("Please enter some text.")
